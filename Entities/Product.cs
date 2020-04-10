93cd62b1eee670e28725a8459a1b932b295f8499
xXAntonioXx <jose_antony11@hotmail.com>
2020-04-09 20:03:41 -0700

    agregando la entidad del producto

---
+++ Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class Product
+    {
+        public int ProductoID { get; set; }
+        public string Descripcion { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public int Disponibilidad { get; set; }
+        public decimal IVA { get; set; }
+        public bool Activo { get; set; }
+    }
+}
