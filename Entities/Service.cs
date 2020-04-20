52e786253b0564895ef340f7dadf169848f529a4
Mario Murillo <mariotinoco85@gmail.com>
2020-04-19 18:33:22 -0700

    se agrego la entidad de servicio

---
+++ Entities/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Service
+    {
+        public int ServicioID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public bool Activo { get; set; }
+    }
+}
