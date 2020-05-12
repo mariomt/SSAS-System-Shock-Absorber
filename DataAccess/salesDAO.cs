3928ac408028c2196ba7c4e8cccb720ebe673a3a
Mario Murillo <mariotinoco85@gmail.com>
2020-05-11 17:10:52 -0700

    se agrego la clase de sales Dao

---
+++ DataAccess/salesDAO.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccess
+{
+    public class salesDAO : DataAccessObjectBase
+    {
+        #region Constructores
+        public salesDAO(IDbConnection pCon)
+            : base(pCon)
+        {
+
+        }
+        public salesDAO()
+            : base(new SqlConnection(ConnectionString.getStringConnection()))
+        {
+
+        }
+
+        #endregion
+        public IDbConnection Connection
+        {
+            get
+            {
+                return this.dbConn;
+            }
+        }
+
+    }
+}
